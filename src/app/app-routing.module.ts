7526b489fad47bdaa4eef9c08cb4895afeadf4c0
theintrigued <58659311+theintrigued@users.noreply.github.com>
2022-05-16 08:35:33 +0300

    Repo Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { TournamentsComponent } from './tournaments/tournaments.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'tournaments', component: TournamentsComponent },
+  { path: 'gallery', component: GalleryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { TournamentsComponent } from './tournaments/tournaments.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactComponent,
+    AboutComponent,
+    TournamentsComponent,
+    ScheduleComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
